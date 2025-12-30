f7c6495fa12dbb7e24bfa469c19a3d674ea3a664
Dimitry Ivaniuta <dzmitry.ivaniuta.services@gmail.com>
2025-12-30 23:33:36 +0100

    Gateway Proxy Toolkit Application

---
+++ src/main/java/com/github/dimitryivaniuta/gateway/GatewayProxyToolkitApplication.java
@@ -0,0 +1,13 @@
+package com.github.dimitryivaniuta.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@EnableCaching
+@SpringBootApplication
+public class GatewayProxyToolkitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayProxyToolkitApplication.class, args);
+    }
+}
